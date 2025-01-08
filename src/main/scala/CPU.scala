87caa058ffd6104ace2f801d781f0dc7510b7e00
Sebastian <sebastian@familienholdt.dk>
2025-01-08 21:02:03 +0100

    test. A lot cleaner code, but takes a very long time to synthesize

--- src/main/scala/CPU.scala
+++ src/main/scala/CPU.scala
@@ -1,6 +1,16 @@
 import chisel3._
 import chisel3.util._
 
+class ControlSignals extends Bundle {
+  // Decode
+  val operand1, operand2 = UInt(32.W)
+  val ALUWB, MemWB, MemStore = Bool()
+  val ALUmode = UInt(4.W)
+  val rd, rs = UInt(5.W)
+  // Execute
+  val ALUResult = UInt(32.W)
+}
+
 class CPU extends Module {
   val io = IO(new Bundle {
     val inst = new Bus()
@@ -21,119 +31,118 @@ class CPU extends Module {
   val instruction = io.inst.readData
 
   val opcode = instruction(6,0) //control
-  val rd = (instruction(11,7)) // write register
-  val funct3 = (instruction(14,12))
-  val rs1 = (instruction(19,15)) //read register 1
-  val rs2 = (instruction(24,20)) //read register 2
-  val funct7 = (instruction(31,25))
+  val rd = instruction(11,7) // write register
+  val funct3 = instruction(14,12)
+  val rs1 = instruction(19,15) //read register 1
+  val rs2 = instruction(24,20) //read register 2
+  val funct7 = instruction(31,25)
 
-  val I_imm = (instruction(31,20))
-  val S_imm = (instruction(31,25) ## instruction(11,7))
-  val B_imm = (instruction(31) ## instruction(7) ## instruction(30,25) ## instruction(11,8) ## 0.U(1.W))
-  val U_imm = (instruction(31,12))
-  val J_imm = (instruction(31) ## instruction(19,12) ## instruction(20) ## instruction(30,21))
+  val I_imm = instruction(31,20)
+  val S_imm = instruction(31,25) ## instruction(11,7)
+  val B_imm = instruction(31) ## instruction(7) ## instruction(30,25) ## instruction(11,8) ## 0.U(1.W)
+  val U_imm = instruction(31,12) ## 0.U(12.W)
+  val J_imm = instruction(31) ## instruction(19,12) ## instruction(20) ## instruction(30,21) ## 0.U(1.W)
 
-  val operand1 = WireDefault(0.U(32.W))
-  val operand2 = WireDefault(0.U(32.W))
-  val ALUWB = WireDefault(false.B)
-  val MemWB = WireDefault(false.B)
-  val ALUmode = WireDefault(0.U(3.W))
+  val id = Wire(new ControlSignals)
+  id.operand1 := 0.U
+  id.operand2 := 0.U
+  id.ALUWB := false.B
+  id.MemWB := false.B
+  id.MemStore := false.B
+  id.ALUmode := 0.U
+  id.rd := rd
+  id.rs := rs2
+  id.ALUResult := 0.U
 
   switch (opcode) {
     is (Opcodes.add) {
-      operand1 := reg(rs1)
-      operand2 := reg(rs2)
-      ALUWB := true.B
-      ALUmode := funct3
+      id.operand1 := reg(rs1)
+      id.operand2 := reg(rs2)
+      id.ALUWB := true.B
+      id.ALUmode := funct7(5) ## funct3
     }
     is (Opcodes.addi) {
-      operand1 := reg(rs1)
-      operand2 := I_imm
-      ALUWB := true.B
-      ALUmode := funct3
+      id.operand1 := reg(rs1)
+      id.operand2 := I_imm
+      id.ALUWB := true.B
+      id.ALUmode := funct3
     }
     is (Opcodes.load) {
-      operand1 := reg(rs1)
-      operand2 := I_imm
-      MemWB := true.B
+      id.operand1 := reg(rs1)
+      id.operand2 := I_imm
+      id.MemWB := true.B
     }
     is (Opcodes.store) {
-      operand1 := reg(rs1)
-      operand2 := S_imm
+      id.operand1 := reg(rs1)
+      id.operand2 := S_imm
+      id.MemStore := true.B
     }
     is (Opcodes.branch) {
       // complicated
     }
     is (Opcodes.lui) {
-      operand1 := U_imm << 12.U
-      operand2 := 0.U
-      ALUWB := true.B
+      id.operand1 := U_imm
+      id.operand2 := 0.U
+      id.ALUWB := true.B
     }
     is (Opcodes.auipc) {
-      operand1 := U_imm << 12.U
-      operand2 := PC
-      ALUWB := true.B
+      id.operand1 := U_imm
+      id.operand2 := PC
+      id.ALUWB := true.B
     }
   }
 
   // Execute
-  val op1 = RegNext(operand1)
-  val op2 = RegNext(operand2)
-  val ALUResult = WireDefault(0.U(32.W))
-
-  switch (funct3) {
+  val ex = RegNext(id)
+  ex.ALUResult := 0.U
+  switch (ex.ALUmode(2,0)) {
     is (0.U) {
-      switch (funct7){
-        is(0.U){ //add
-          ALUResult := op1 + op2
-        }
-        is(32.U){ //sra
-          ALUResult := op1 - op2
-        }
+      when (ex.ALUmode(3)) {
+        ex.ALUResult := ex.operand1 - ex.operand2
+      } .otherwise {
+        ex.ALUResult := ex.operand1 + ex.operand2
       }
     }
     is (1.U){
-      ALUResult := op1 << op2(5,0)
+      ex.ALUResult := ex.operand1 << ex.operand2(5,0)
     }
     is (2.U){
-      ALUResult := op1.asSInt < op2.asSInt
+      ex.ALUResult := ex.operand1.asSInt < ex.operand2.asSInt
     }
     is (3.U){
-      ALUResult := op1 < op2
+      ex.ALUResult := ex.operand1 < ex.operand2
     }
     is (4.U){
-      ALUResult := op1 ^ op2
+      ex.ALUResult := ex.operand1 ^ ex.operand2
     }
     is (5.U){ //check if SInt is on srl or sra
-      switch (funct7){
-        is(0.U){ //srl
-          ALUResult := (op1.asSInt >> op2(5,0)).asUInt
-        }
-        is(32.U){ //sra
-          ALUResult := op1 >> op2(5,0) //test if 5,0 or 4,0. (4,0 >> 31x ikke 32x)
-        }
+      when (ex.ALUmode(3)) { //sra
+        ex.ALUResult := ex.operand1 >> ex.operand2(5,0) //test if 5,0 or 4,0. (4,0 >> 31x ikke 32x)
+      } .otherwise { //srl
+        ex.ALUResult := (ex.operand1.asSInt >> ex.operand2(5,0)).asUInt
       }
     }
     is (6.U){
-      ALUResult := op1 | op2
+      ex.ALUResult := ex.operand1 | ex.operand2
     }
     is (7.U){
-      ALUResult := op1 & op2
+      ex.ALUResult := ex.operand1 & ex.operand2
     }
   }
 
   // Memory
-  io.data.addr := ALUResult
-  io.data.writeData := 0.U
-  io.data.write := false.B
+  val mem = RegNext(ex)
+  io.data.addr := mem.ALUResult
+  io.data.writeData := reg(mem.rs)
+  io.data.write := mem.MemStore
 
   // Writeback
-  val destination = RegNext(RegNext(RegNext(rd)))
-  when (destination =/= 0.U) {
-    when (RegNext(RegNext(RegNext(ALUWB)))) {
-      reg(destination) := RegNext(RegNext(ALUResult))
-    } .elsewhen (RegNext(RegNext(RegNext(MemWB)))) {
-      reg(destination) := io.data.readData
+  val wb = RegNext(mem)  
+  when (wb.rd =/= 0.U) {
+    when (wb.ALUWB) {
+      reg(wb.rd) := wb.ALUResult
+    } .elsewhen (wb.MemWB) {
+      reg(wb.rd) := io.data.readData
     }
   }
 
