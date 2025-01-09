4f75642bf7e81b02133cb92c8e447080e654a7d3
karl3009 <s235481@dtu.dk>
2025-01-09 11:25:24 +0100

    ResetTest for led

---
+++ src/test/scala/ResetTest.scala
@@ -0,0 +1,20 @@
+import chisel3._
+import chiseltest._
+import org.scalatest.flatspec.AnyFlatSpec
+
+class ResetTest extends AnyFlatSpec with ChiselScalatestTester {
+  "Top" should "pass" in {
+    test(new Top()) { dut =>
+      println("\n---Start of reset test:---")
+      println("Led value (start): " + dut.io.led(0).peekBoolean())
+      dut.reset.poke(true.B)
+      dut.clock.step(1)
+      dut.reset.poke(false.B)
+      dut.clock.step(2)
+      println("Led value (2 steps): " + dut.io.led(0).peekBoolean())
+      dut.clock.step(1)
+      println("Led value (3 steps): " + dut.io.led(0).peekBoolean())
+      dut.io.led(0).expect(true.B)
+    }
+  }
+}
