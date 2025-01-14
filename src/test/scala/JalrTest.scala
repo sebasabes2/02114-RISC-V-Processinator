245e61c57407f40bcc7bc70929bb42a213f11f5d
karl3009 <s235481@dtu.dk>
2025-01-14 12:19:59 +0100

    Beq,Jal & Jalr Tests

--- src/test/scala/JalrTest.scala
+++ src/test/scala/JalrTest.scala
@@ -5,34 +5,25 @@ import org.scalatest.flatspec.AnyFlatSpec
 class JalrTest extends AnyFlatSpec with ChiselScalatestTester {
   "CPU " should "pass" in {
     test(new CPU()) { dut =>
-      println("PC: "+ dut.io.PC.peekInt())
-      dut.io.inst.readData.poke(0x01400113.U) //addi x2 x0 20
-      dut.clock.step(1)
-      dut.io.inst.readData.poke(0x00000013.U) //no op
-      dut.clock.step(3)
-      println("PC: "+ dut.io.PC.peekInt())
-      println("x3: "+ dut.io.reg(3).peekInt())
-
-      dut.io.inst.readData.poke(0x014101e7.U) //jalr x3 x2 20
-      dut.clock.step(1)
-      dut.io.inst.readData.poke(0x00000013.U) //no op
-      println("PC: "+ dut.io.PC.peekInt())
-      println("x3: "+ dut.io.reg(3).peekInt())
-      dut.clock.step(1)
-      println("PC: "+ dut.io.PC.peekInt())
-      println("x3: "+ dut.io.reg(3).peekInt())
-      dut.clock.step(1)
-      println("PC: "+ dut.io.PC.peekInt())
-      println("x3: "+ dut.io.reg(3).peekInt())
-      dut.clock.step(1)
-      println("PC: "+ dut.io.PC.peekInt())
-      println("x3: "+ dut.io.reg(3).peekInt())
-      dut.clock.step(1)
-      println("PC: "+ dut.io.PC.peekInt())
-      println("x3: "+ dut.io.reg(3).peekInt())
-
-      //make expects and remove println
-
+      val array = Array(
+        0x01c004e7, //jalr x0 x0 28
+        0x00000013, //addi x0 x0 0
+        0x00000013, //addi x0 x0 0
+        0x00000013, //addi x0 x0 0
+        0x00100093, //addi x1 x0 1
+        0x00100113, //addi x2 x0 1
+        0x00100193, //addi x3 x0 1
+        0x00100213, //addi x4 x0 1
+        0x00100293, //addi x5 x0 1
+        0x00100313, //addi x6 x0 1
+      )
+      RunProgram(dut,array)
+      for (i <- 1 until 4){
+        dut.io.reg(i).expect(0)
+      }
+      for (i <-4 until 7){
+        dut.io.reg(i).expect(1)
+      }
     }
   }
 }
