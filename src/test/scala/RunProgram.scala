d697771892f9387ad9ec0e3a8d92643e711b481c
Sebastian <sebastian@familienholdt.dk>
2025-01-14 13:44:40 +0100

    .

--- src/test/scala/RunProgram.scala
+++ src/test/scala/RunProgram.scala
@@ -9,14 +9,13 @@ object RunProgram {
       dut.clock.step(1)
       val index = addr / 4
       if (index >= program.length) {
-    
-      val addr = dut.io.inst.addr.peekInt()
         // Finish remaining stages
         dut.io.inst.readData.poke(0x00000013.U) // nop
         dut.clock.step(5)
         return
       }
       dut.io.inst.readData.poke(program(index.intValue()).U)
+      // If program is stuck
       i = i + 1
       if (i >= 100000) {
         println("Reached max clock cycles of " + i)
