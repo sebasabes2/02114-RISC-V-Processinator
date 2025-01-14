5d074705a66759aaebebbfca107935387b0ceaf3
Sebastian <sebastian@familienholdt.dk>
2025-01-14 12:43:20 +0100

    change to JalTest

--- src/test/scala/JalTest.scala
+++ src/test/scala/JalTest.scala
@@ -6,24 +6,25 @@ class JalTest extends AnyFlatSpec with ChiselScalatestTester {
   "CPU " should "pass" in {
     test(new CPU()) { dut =>
       val array = Array(
-        0x01c004ef, //jal x0 x0 28
-        0x00000013, //addi x0 x0 0
-        0x00000013, //addi x0 x0 0
-        0x00000013, //addi x0 x0 0
-        0x00100093, //addi x1 x0 1
-        0x00100113, //addi x2 x0 1
-        0x00100193, //addi x3 x0 1
-        0x00100213, //addi x4 x0 1
-        0x00100293, //addi x5 x0 1
-        0x00100313, //addi x6 x0 1
+        0x01c004ef, // jal x9 28
+        0x00000013, // nop
+        0x00000013, // nop
+        0x00000013, // nop
+        0x00100093, // addi x1 x0 1
+        0x00100113, // addi x2 x0 1
+        0x00100193, // addi x3 x0 1
+        0x00100213, // addi x4 x0 1
+        0x00100293, // addi x5 x0 1
+        0x00100313, // addi x6 x0 1
       )
       RunProgram(dut,array)
-      for (i <- 1 until 4){
+      for (i <- 1 to 3){
         dut.io.reg(i).expect(0)
       }
-      for (i <-4 until 7){
+      for (i <- 4 to 6){
         dut.io.reg(i).expect(1)
       }
+      dut.io.reg(9).expect(4)
     }
   }
 }
