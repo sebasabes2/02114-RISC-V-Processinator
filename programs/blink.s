3ba31279497e82d58ce91bac14d6a4d0f6063cc3
Sebastian <sebastian@familienholdt.dk>
2025-01-16 14:27:30 +0100

    added echo program

--- programs/blink.s
+++ programs/blink.s
@@ -1,3 +1,4 @@
+.text
 li x1, 0        # loop index 
 li x2, 0x6cf000 # max loop
 li x3, 1        # LED on
@@ -7,24 +8,17 @@ li x6, 0x3000   # UART address
 
 loop1:
 addi x1, x1, 1
-nop
-nop
-nop
 blt x1, x2, loop1
 nop
 nop
-nop
 
+lw x5, 0(x6) # receive character
 sw x3, 0(x4) # turn on LED
 sw x5, 0(x6) # send 'O' to UART
 
 loop2:
 addi x1, x1, -1
-nop
-nop
-nop
 blt x0, x1, loop2
-nop
 nop
 nop
 
