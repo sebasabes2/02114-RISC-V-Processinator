5782671984af88d7e8eb1e06d43841dcd68eb865
karl3009 <s235481@dtu.dk>
2025-01-20 11:35:16 +0100

    it works :D

--- src/main/scala/peripherals/ButtonController.scala
+++ src/main/scala/peripherals/ButtonController.scala
@@ -8,40 +8,62 @@ class ButtonController(start: Int, size: Int) extends Module {
     val btn = Input(Vec(4, Bool()))
   })
 
-  val btnU = RegInit(false.B)
-  val btnR = RegInit(false.B)
-  val btnD = RegInit(false.B)
-  val btnL = RegInit(false.B)
-
-
-
-
-
-  when(io.btn(0)){
-    btnU := true.B
-  }
-  when(io.btn(1)){
-    btnL := true.B
-  }
-  when(io.btn(2)){
-    btnR := true.B
-  }
-  when(io.btn(3)){
-    btnD := true.B
-  }
-
-
+  //BUS
   val width = log2Up(size)
   val page = io.bus.addr(31, width)
 
+  //sync
+  val btnSyncU = RegNext(RegNext(io.btn(0)))
+  val btnSyncL = RegNext(RegNext(io.btn(1)))
+  val btnSyncR = RegNext(RegNext(io.btn(2)))
+  val btnSyncD = RegNext(RegNext(io.btn(3)))
+
+  //debounce
+  val CLOCK_FREQ = 100000000 // 100 MHz
+  val DEBOUNCE_PERIOD = 20000 // 20 ns
+  val DEBOUNCE_COUNTER_MAX = CLOCK_FREQ / 1000000 * DEBOUNCE_PERIOD
+  val debounce_counter = RegInit(0.U(log2Up(DEBOUNCE_COUNTER_MAX).W))
+  val tick = debounce_counter === (DEBOUNCE_COUNTER_MAX-1).U
+
+  val btnDebouncedU = Reg(Bool())
+  val btnDebouncedL = Reg(Bool())
+  val btnDebouncedR = Reg(Bool())
+  val btnDebouncedD = Reg(Bool())
+
+  debounce_counter := debounce_counter + 1.U
+  when(debounce_counter === (DEBOUNCE_COUNTER_MAX-1).U){
+    debounce_counter := 0.U
+    btnDebouncedU := btnSyncU
+    btnDebouncedL := btnSyncL
+    btnDebouncedR := btnSyncR
+    btnDebouncedD := btnSyncD
+  }
+
+  val shiftRegU = RegInit(0.U(3.W))
+  val shiftRegL = RegInit(0.U(3.W))
+  val shiftRegR = RegInit(0.U(3.W))
+  val shiftRegD = RegInit(0.U(3.W))
+
+  when(debounce_counter === (DEBOUNCE_COUNTER_MAX-1).U) {
+    shiftRegU := shiftRegU(1, 0) ## btnDebouncedU
+    shiftRegL := shiftRegL(1, 0) ## btnDebouncedL
+    shiftRegR := shiftRegR(1, 0) ## btnDebouncedR
+    shiftRegD := shiftRegD(1, 0) ## btnDebouncedD
+  }
+
+  //majority
+
+  //works btnSync, io.btn(x), btnDebounce
+  //check shiftReg
+  val btnCleanU = ( shiftRegU (2) & shiftRegU (1)) | ( shiftRegU (2) & shiftRegU (0)) | ( shiftRegU (1) & shiftRegU (0))
+  val btnCleanL = ( shiftRegL (2) & shiftRegL (1)) | ( shiftRegL (2) & shiftRegL (0)) | ( shiftRegL (1) & shiftRegL (0))
+  val btnCleanR = ( shiftRegR (2) & shiftRegR (1)) | ( shiftRegR (2) & shiftRegR (0)) | ( shiftRegR (1) & shiftRegR (0))
+  val btnCleanD = ( shiftRegD (2) & shiftRegD (1)) | ( shiftRegD (2) & shiftRegD (0)) | ( shiftRegD (1) & shiftRegD (0))
+
   val valid = RegNext(page === (start/size).U)
   when (valid) {
-    //                  4       3       2       1
-    io.bus.readData := btnU ## btnR ## btnD ## btnL
-    btnU := false.B
-    btnR := false.B
-    btnD := false.B
-    btnL := false.B
+    //                     4             3           2           1
+    io.bus.readData := btnCleanL ## btnCleanD ## btnCleanR ## btnCleanU
   } .otherwise {
     io.bus.readData := 0.U
   }
