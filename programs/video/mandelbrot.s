bad316d4b90722789eebe6cdd433528332464020
Sebastian <sebastian@familienholdt.dk>
2025-01-22 23:35:27 +0100

    adjusted zoom of mandelbrot program

--- programs/video/mandelbrot.s
+++ programs/video/mandelbrot.s
@@ -24,25 +24,19 @@ li s3, 0 # y value
 yloop:
 li s2, 0 # x value
 xloop:
+
 # loop pixels
-
-
-# map x and y to fixed points by left shift by 5
-
 mv a0, s2 # move x to x input
 mv a1, s3 # move y to y input
 
-slli a0, a0, 5
-slli a1, a1, 5
+addi a0, a0, -240
+addi a1, a1, -120
+
+slli a0, a0, 6
+slli a1, a1, 6
 
 jal ra, iterate
 mv a2, a0
-
-# li t0, 100
-# li a2, 0x3f  # load color with white
-# blt s2, t0, label1 
-# li a2, 0  # load color with black
-# label1:
 
 mv a0, s2 # move x to x input
 mv a1, s3 # move y to y input
@@ -66,23 +60,6 @@ add t1, t1, t0
 sw a2, 0(t1)
 jalr x0, 0(ra)
 
-# iterate: # old "iterate". displays box in the middle of screen
-# li t0, 50
-# li t1, 150
-# blt a0, t0, white
-# blt t1, a0, white
-# blt a1, t0, white
-# blt t1, a1, white
-# black:
-# li a0, 0
-# jalr x0, 0(ra)
-# white:
-# li a0, 0x3f
-# jalr x0, 0(ra)
-
-# 
-
-
 iterate:
 # a0 = fixed real (input)
 # a1 = fixed imag (input)
@@ -92,8 +69,8 @@ iterate:
 # a7 = temp
 # t0 = fixed real
 # t1 = fixed imag
-# t2 = fixed realq
-# t3 = fixed imagq
+# t2 = fixed realsq
+# t3 = fixed imagsq
 # t4 = int i
 # t5 = int 50
 # t6 = fixed 4.0
@@ -109,27 +86,28 @@ mv a6, ra
 mv t0, a0
 mv t1, a1
 
-li t4, 0         # i = 0
-li t5, 50        # t5 = 50
-li t6, 4         # t6 = (int) 4
+# initialize i, 50, 4.0
+li t4, 0        # i = 0
+li t5, 64       # t5 = 50
+li t6, 4        # t6 = (int) 4
 slli t6, t6, 13 # t6 = (fixed) 4.0
 
 # for (i = 0; i < 50; i ++)
 iterate_loop:
 
-# realq = real * real
+# realsq = real * real
 mv a0, t0
 mv a1, t0
 jal ra, mult_fixed
 mv t2, a2
 
-# imagq = imag * imag
+# imagsq = imag * imag
 mv a0, t1
 mv a1, t1
 jal ra, mult_fixed
 mv t3, a2
 
-## if (realq + imagq) > (fixed) 4 then break 
+## if (realsq + imagsq) > (fixed) 4 then break 
 add a7, t2, t3
 bge a7, t6, iterate_return
 
@@ -141,7 +119,7 @@ mv t1, a2
 slli t1, t1, 1
 add t1, t1, a5
 
-# real = realq - imagq + real0
+# real = realsq - imagsq + real0
 sub t0, t2, t3
 add t0, t0, a4
 
@@ -161,35 +139,10 @@ mv ra, a6
 jalr x0, 0(ra)
 
 
+# Multiplication subroutine:
 
 mult_fixed:
-
-# # check0 if a0 is 0 return 0
-# li a2, 0
-# bne x0, a0, mult_check1
-# jalr x0, 0(ra)
-
-# mult_check1:
-# # check1 if a0 is negative and a1 is negative, invert both and do pos_pos
-# bge a0, x0, mult_check2
-# bge a1, x0, mult_check2
-# sub a0, x0, a0 # a0 = -a0
-# sub a1, x0, a1 # a1 = -a1
-# beq x0, x0, mult_fixed_pos_pos
-
-# mult_check2:
-
-
-# mult_fixed_pos_pos:
-# add a2, a2, a1
-# addi a0, a0, -1
-# bne x0, a0, mult_fixed_pos_pos
-# srai a2, a2, 13
-# jalr x0, 0(ra)
-
-
 li a2, 0
-
 mult_fixed_loop:
 beq a0, x0, mult_fixed_return
 andi a3, a0, 1
@@ -202,4 +155,3 @@ beq x0, x0, mult_fixed_loop
 mult_fixed_return:
 srai a2, a2, 13
 jalr x0, 0(ra)
-
