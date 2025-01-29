3356b9a80ab4f201c44b1ebe4869a783d2135cbe
Sebastian <sebastian@familienholdt.dk>
2025-01-29 17:30:49 +0100

    removed UART part of blink.s program

--- programs/blink.s
+++ programs/blink.s
@@ -3,8 +3,6 @@ li x1, 0        # loop index
 li x2, 0x6cf000 # max loop
 li x3, 1        # LED on
 li x4, 0x2000   # LED address
-li x5, 'O'
-li x6, 0x3000   # UART address
 
 loop1:
 addi x1, x1, 1
@@ -12,9 +10,7 @@ blt x1, x2, loop1
 nop
 nop
 
-lw x5, 0(x6) # receive character
 sw x3, 0(x4) # turn on LED
-sw x5, 0(x6) # send 'O' to UART
 
 loop2:
 addi x1, x1, -1
